866de18bbae2dfb8790dc1202c0689db375f2856
fillerInk <thatonephysicsnerd@gmail.com>
2020-06-08 20:13:58 +0530

    Button and number field

--- app/src/main/java/com/example/callmemaybe/MainActivity.java
+++ app/src/main/java/com/example/callmemaybe/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.callmemaybe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Button Njekki", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
