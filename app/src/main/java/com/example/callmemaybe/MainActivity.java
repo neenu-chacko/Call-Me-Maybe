af8ed5dc3a283eac01ab4cb7f804fe5579d806a7
NCM <neenuchacko@live.in>
2020-06-20 20:02:19 +0530

    Work aai

--- app/src/main/java/com/example/callmemaybe/MainActivity.java
+++ app/src/main/java/com/example/callmemaybe/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.callmemaybe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        final EditText text1 = findViewById(R.id.numberField);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button Njekki", Toast.LENGTH_SHORT).show();
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/+91" + text1.getText()));
+               startActivity(browserIntent);
+
+              // Toast.makeText(getApplicationContext(),text1.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
